7f69ef6e3f734b27815d80e9f24100ad6682cb17
WUYUEZONG <w.yzong@outlook.com>
2023-07-31 14:57:48 +0800

    format

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="board">
-    <h2 class="title">Johnathan Specter</h2>
-    <ul>
-        <li v-for="i in lists" :key="i">
-            <span>{{ i }}</span>
-        </li>
-    </ul>
-    <span>
-        <button>Get in touch</button>
-        <img src="@/assets/HamburgerMenu.svg" alt="" />
-    </span>
-  </div>
+    <div class="board">
+        <h2 class="title">Johnathan Specter</h2>
+        <ul>
+            <li v-for="i in lists" :key="i">
+                <span>{{ i }}</span>
+            </li>
+        </ul>
+        <span>
+            <button>Get in touch</button>
+            <img src="@/assets/HamburgerMenu.svg" alt="" />
+        </span>
+    </div>
 </template>
 
 <script setup>
@@ -25,23 +25,24 @@ const lists = ['Articles', 'Chats', 'Awards', 'About']
 }
 
 ul {
-  list-style: none;
-  display: flex;
-  gap: 20px;  
+    list-style: none;
+    display: flex;
+    gap: 20px;
 }
 
 img {
-        display: none;
+    display: none;
 }
 
 @media (max-width: 767px) {
-    ul, button {
+
+    ul,
+    button {
         display: none;
     }
+
     img {
         display: block;
     }
 }
-
-
 </style>
